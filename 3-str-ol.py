77e166cce02d7b12698541e846ca1895d0794e08
BufteacEcaterina03 <70895514+BufteacEcaterina03@users.noreply.github.com>
2020-12-08 23:55:30 +0200

    Add files via upload

---
+++ 3-str-ol.py
@@ -0,0 +1,5 @@
+n=int(input("Introduceti nr de ani :"))
+l=12*n
+z=365*n
+o=24*z
+print (" In " ,n , "ani sunt" , l, "luni " ,z ,"zile", o, "ore")
---
+++ 4-str-ol.py
@@ -0,0 +1,13 @@
+lp=48
+#lungimea gardului patratului
+ld=9
+#latimea dreptunghiului
+lp=48/4
+#latura patratului
+Ap=lp**2
+#aria
+ld1=Ap/ld
+#latura dreptunghiului
+d=(lp+ld1)*2
+#lungimea gardului dreptunghiului
+print ("Lungimea gardului ce imprejmuieste acest lot :",d)
